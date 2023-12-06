61721291992f0bb59d3ff1b027b0755de39a4bd2
Antonia <as54033@fer.hr>
2023-12-06 20:10:15 +0100

    feat: add distinct-2 score

--- eval.py
+++ eval.py
@@ -1,6 +1,7 @@
 import os
 import json
 from nltk.translate.bleu_score import sentence_bleu
+from rouge_score import rouge_scorer
 
 import util
 
@@ -18,8 +19,22 @@ for filename in os.listdir(dir):
         llmAnswers = json.load(file)
 
     for i in range(len(llmAnswers)):
-        bleu_score = sentence_bleu(tokenizedAnswers[i], llmAnswers[i]['answer'].split())
-        llmAnswers[i]['evaluation'] = {'bleu_score': bleu_score}
+        llm_generated_tokens = llmAnswers[i]['answer'].split()
+        #BLEU
+        bleu_score = sentence_bleu(tokenizedAnswers[i], llm_generated_tokens)
+        llmAnswers[i]['evaluation']['bleu_score'] = bleu_score
+
+        #rouge-l
+        scorer = rouge_scorer.RougeScorer(['rougeL'], use_stemmer=True)
+
+        avgRouge = util.calculateAverageRouge(scorer, llmAnswers[i]['answer'], answers[i]['answers'])
+        llmAnswers[i]['evaluation']['rouge_score']['precision'] = avgRouge['precision']
+        llmAnswers[i]['evaluation']['rouge_score']['recall'] = avgRouge['recall']
+        llmAnswers[i]['evaluation']['rouge_score']['f1'] = avgRouge['f1']
+
+        #distinct-2
+        llmAnswers[i]['evaluation']['diversity'] = util.distinct_2_score(llm_generated_tokens)
+
 
     with open(file_path, 'w') as file:
         json.dump(llmAnswers, file, indent=2)
--- util.py
+++ util.py
@@ -1,3 +1,5 @@
+from nltk import ngrams
+
 def tokenizeHumanAnswers(answers):
     tokenizedAnswers = []
     
@@ -8,3 +10,31 @@ def tokenizeHumanAnswers(answers):
         tokenizedAnswers.append(pom)
 
     return tokenizedAnswers
+
+
+def calculateAverageRouge(scorer, hyps, refs):
+    avgRouge = {'precision': 0.0, 'recall': 0.0, 'f1': 0.0}
+
+    for ref in refs:
+        scores = scorer.score(ref, hyps)
+        avgRouge['precision'] += scores['rougeL'].precision
+        avgRouge['recall'] += scores['rougeL'].recall
+        avgRouge['f1'] += scores['rougeL'].fmeasure
+
+    num_references = len(refs)
+    if num_references > 0:
+        avgRouge['precision'] /= num_references
+        avgRouge['recall'] /= num_references
+        avgRouge['f1'] /= num_references
+
+    return avgRouge
+
+
+def distinct_2_score(response):
+    if len(response) < 2:
+        return 0.0 
+
+    bigrams = list(ngrams(response, 2))
+    distinct_2 = len(set(bigrams)) / len(bigrams)
+
+    return distinct_2 
